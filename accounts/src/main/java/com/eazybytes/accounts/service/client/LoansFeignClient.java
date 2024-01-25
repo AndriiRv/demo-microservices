ade24ae552c6fa7134d512af92170bb6fb87225b
Andrii Reva <rewa.andrei2010@gmail.com>
2024-01-25 18:53:26 +0200

    demo-microservices: fix fallback for FeignClient in Accounts microservice.
    - Fixed fallback for FeignClient in Accounts microservice.

--- accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "loans", fallback = LoansFeignClient.class)
+@FeignClient(name = "loans", fallback = LoansFallback.class)
 public interface LoansFeignClient {
 
     @GetMapping(value = "/api/fetch", consumes = "application/json")
